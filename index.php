cc87d8a900d49fbceecffbd11bb880ce2254ac68
Neha Reddy <nehareddy200709@gmail.com>
2026-09-16 15:09:47 +0530

    init pract and setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    echo"I likeeeeeee pizza <br>";
+    echo"Its really goof";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>Hello</button>
+</body>
+</html>
