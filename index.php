1c54b0a8777e03aab922d81a12f80646e4d321f7
Neha Reddy <nehareddy200709@gmail.com>
2026-09-16 18:31:08 +0530

    Radio

--- index.php
+++ index.php
@@ -1,44 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action = "index.php" method = "post">
+        <input type="radio" name="f1" value="mercedes">Mercedes<br>
+        <input type="radio" name="f1" value="rb">RedBull<br>
+        <input type="radio" name="f1" value="vcarb">VCARB<br>
+        <input type="radio" name="f1" value="mcl">Mclaren<br>
+        <input type="submit" name="confirm" value ="Submit"><br>
+    </form>
+</body>
+</html>
 
 <?php 
+    if(isset($_POST["confirm"])){
+
+        if(isset($_POST["f1"])){
+            $f1 = $_POST["f1"];
+            echo $f1 . "<br>";
+
+            if($f1 == "mercedes"){
+                echo "You sel merc <br>";
+            }elseif($f1 == "rb"){
+                echo "You sel redbull <br>";
+            }elseif($f1 == "vcarb"){
+                echo "You sel vcarb <br>";
+            }elseif($f1 == "mcl"){
+                echo "You sel mcl <br>";
+            }
+        }
+        
+    }
     
-    // $capital = array(
-    //     "usa" => "dc",
-    //     "india" => "delhi",
-    //     "china" => "beijing",
-    //     "monaco" => "monaco"
-    // );
-    // echo $capital["usa"] . "<br>";
-
-    // foreach($capital as $key => $val ){
-    //     echo "{$key} = {$val} <br>";
-    // }
-
-    $username = "user";
-    if(isset($username)){
-        echo "This is set <br>";
-    }else{
-        echo "This is not set <br>";
-    }
-
-    $username = false;
-    if(empty($username)){
-        echo "This is empty <br>";
-    }else{
-        echo "This is not empty <br>";
-    }
-
-    $username = null;
-    if(empty($username)){
-        echo "This is empty <br>";
-    }else{
-        echo "This is not empty <br>";
-    }
-
-    $username ;
-    if(empty($username)){
-        echo "This is empty <br>";
-    }else{
-        echo "This is not empty <br>";
-    }
         
 ?>
