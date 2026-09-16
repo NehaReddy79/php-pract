182e495316b59468b2d07572c6ea2d67624577f8
Neha Reddy <nehareddy200709@gmail.com>
2026-09-16 16:27:51 +0530

    Math function

--- index.php
+++ index.php
@@ -7,17 +7,35 @@
 </head>
 <body>
     <form action="index.php" method="post">
-        <label>Username : </label>
-        <input type="text" name="username"><br>
-        <label>Passowrd : </label>
-        <input type ="password" name="password"><br>
-        <button type="submit">Submit</button>
+        <label>radius : </label>
+        <input type="text" name="r">
+        <input type="submit" value="total">
     </form>
 </body>
 </html>
 
 <?php 
-    echo $_POST["username"] . "<br>";
-    echo "{$_POST["password"]} <br>";
-
+    
+    // $x = $_POST["x"];
+    // echo "{$x} <br>";
+    // $total = abs($x);
+    // echo "{$total} <br>";
+    // $total = round($x);
+    // echo "{$total} <br>";
+    // $total = ceil($x);
+    // echo "{$total} <br>";
+    // $total = floor($x);
+    // echo "{$total} <br>";
+    // $total = pow($x , $x);
+    // echo "{$total} <br>";
+    // echo "{$abs($x)} <br>";
+    $r  = $_POST["r"];
+    echo "Radius {$r} <br>";
+    $area = pi() * $r * $r;
+    $circ = 2* pi() * $r;
+    $vol = (4 * pi() * $r)/3;
+    
+    echo "Area {$area} <br>";
+    echo "circ {$circ} <br>";
+    echo "vol {$vol} <br>";
 ?>
