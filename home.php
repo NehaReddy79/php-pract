3f448593e1cd9665d7e599d7b8c7f869e0841d88
Neha Reddy <nehareddy200709@gmail.com>
2026-09-17 09:38:05 +0530

    Sessions

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php 
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="home.php" method="post">
+        
+        <input type="submit" value="Logout" name="logout">
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_POST["logout"])){
+        session_destroy();
+        header("Location: index.php");
+    }
+    
+?>
--- index.php
+++ index.php
@@ -1,9 +1,5 @@
-
-
 <?php 
-    include("header.html");
-    
-    
+    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,10 +9,28 @@
     <title>Document</title>
 </head>
 <body>
-    This is the body of the website<br>
+    <form action="index.php" method="post">
+        username : <br>
+        <input type="text" name="username"><br>
+        password : <br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Login" name="login">
+    </form>
+    
 </body>
 </html>
 
 <?php 
-    include("footer.html");
+    if(isset($_POST["login"])){
+       
+        
+        if(!empty($_POST["password"]) && !empty($_POST["username"])){
+            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["password"] = $_POST["password"];
+
+            header("Location: home.php");
+        }else{
+            echo "Missing fields <br>";
+        }
+    }
  ?>
